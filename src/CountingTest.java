369e1c00d37eebff796ed2e4fe3aa68aed379bb2
Agnieszka Majak <agamajak@wp.pl>
2020-04-22 21:20:57 +0200

    created method countingWithWhileLoop

--- src/CountingTest.java
+++ src/CountingTest.java
@@ -1,2 +1,8 @@
+import logic.CountingLoop;
+
 class CountingTest {
+    public static void main(String[] args) {
+        CountingLoop.countingWithWhileLoop();
+
+    }
 }
--- src/logic/CountingLoop.java
+++ src/logic/CountingLoop.java
@@ -1,4 +1,14 @@
 package logic;
 
 public class CountingLoop {
+
+    public static void countingWithWhileLoop() {
+        double i = 0;
+        while (i <= 3.1) {
+            System.out.printf("%.1f\n", i);
+            i += 0.1;
+        }
+    }
+
+
 }
